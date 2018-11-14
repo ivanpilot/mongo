b201a0a37abecccd5b8c3a6bc86274dc32c66f99
ip61584 <ip61584@imceu.eu.ssmb.com>
2018-11-14 21:19:57 +0000

    added deleted test and currently working on the updating tests

--- test/delete.js
+++ test/delete.js
@@ -14,17 +14,37 @@ describe('Deleting a user', () => {
 
   it('model instance remove', (done) => {
     joe.remove()
-      .then(() => {
-
+      .then(() => User.find({ name: 'Joe'}))
+      .then((users) => {
+        assert(users.length === 0);
+        done();
       })
   })
+
   it('class method remove', (done) => {
-
+    User.remove({name: 'Joe'})
+      .then(() => User.find({ name: 'Joe'}))
+      .then((users) => {
+        assert(users.length === 0);
+        done();
+      })
   })
+
   it('class method findAndRemove', (done) => {
-
+    User.findOneAndRemove({name: 'Joe'})
+      .then(() => User.find({ name: 'Joe'}))
+      .then((users) => {
+        assert(users.length === 0);
+        done();
+      })
   })
-  it('class method findByIdAndRemove', (done) => {
 
+  it('class method findByIdAndRemove', (done) => {
+    User.findByIdAndRemove({_id: joe._id})
+      .then(() => User.find({ name: 'Joe'}))
+        .then((users) => {
+          assert(users.length === 0);
+          done();
+        })
   })
 })
---
+++ test/update.js
@@ -0,0 +1,29 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Updating records', () => {
+  let joe;
+
+  beforeEach((done) => {
+    joe = new User({name: 'Joe'})
+    joe.save()
+      .then(() => done());
+  })
+
+  it('instance type using set n save', (done) => {
+    console.log(joe)
+  })
+
+  // it('', (done) => {
+
+  // })
+  // it('', (done) => {
+
+  // })
+  // it('', (done) => {
+
+  // })
+  // it('', (done) => {
+
+  // })
+})
